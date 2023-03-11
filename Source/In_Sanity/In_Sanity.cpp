aa2f937f04da8dc6f2a4e9e71451c8c9ce7ef223
745th <72306497+745th@user.noreply.github.com>
2023-03-11 17:30:04 +0100

    Main

---
+++ Source/In_Sanity/In_Sanity.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "In_Sanity.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, In_Sanity, "In_Sanity" );
---
+++ Source/In_Sanity/In_SanityGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "In_SanityGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IN_SANITY_API AIn_SanityGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
