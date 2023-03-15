633fc3f9062291d52af3f5eff10b2ea9b795acd6
745th <72306497+745th@user.noreply.github.com>
2023-03-15 11:52:11 +0100

    v1.0 without patch

--- Source/In_Sanity/In_Sanity.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "In_Sanity.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, In_Sanity, "In_Sanity" );
--- Source/In_Sanity/In_SanityGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "In_SanityGameModeBase.h"
-
